915c843aa562d12c1ebf49e4f2ee2eef2423446e
James Munns <james@onevariable.com>
2022-04-03 13:33:09 +0200

    Add docs to the allocation types

--- firmware/kernel/src/alloc.rs
+++ firmware/kernel/src/alloc.rs
@@ -11,22 +11,37 @@ use linked_list_allocator::Heap;
 
 pub static HEAP: AHeap = AHeap::new();
 
+// TODO: This should probably live in a specified linker section.
 static HEAP_BUF: HeapStorage = HeapStorage::new();
 static FREE_Q: FreeQueue = FreeQueue::new();
+
+// Size is roughly ptr + size + align, so about 3 words.
 const FREE_Q_LEN: usize = 128;
 
+/// An Anachro Heap item
 pub struct AHeap {
     state: AtomicU8,
     heap: UnsafeCell<MaybeUninit<Heap>>,
 }
 
+// SAFETY: Safety is checked through the `state` member, which uses
+// atomic operations to ensure the data is initialized and exclusively
+// accessed.
 unsafe impl Sync for AHeap {}
 
 impl AHeap {
+    /// The AHeap is uninitialized. This is the default state.
     const UNINIT: u8 = 0;
-    const INIT_IDLE: u8 = 1;
-    const INIT_BUSY: u8 = 2;
 
+    /// The AHeap is initialized, and no `HeapGuard`s are active.
+    const INIT_IDLE: u8 = 1;
+
+    /// The AHeap is "locked", and cannot be retrieved. In MOST cases
+    /// this also means the heap is initialized, except for the brief
+    /// time while the heap is being initialized.
+    const BUSY_LOCKED: u8 = 2;
+
+    /// Create a new, uninitialized AHeap
     pub const fn new() -> Self {
         Self {
             state: AtomicU8::new(Self::UNINIT),
@@ -34,18 +49,27 @@ impl AHeap {
         }
     }
 
+    /// Initialize the AHeap.
+    ///
+    /// This takes care of initializing all contained memory and tracking variables.
+    /// This function should only be called once, and should be called prior to using
+    /// the AHeap.
+    ///
+    /// Returns `Ok(())` if initialization was successful. Returns `Err(())` if the
+    /// AHeap was previously initialized.
     pub fn init(&self) -> Result<(), ()> {
         self.state
             .compare_exchange(
                 Self::UNINIT,
-                Self::INIT_BUSY,
+                Self::BUSY_LOCKED,
                 Ordering::SeqCst,
                 Ordering::SeqCst,
             )
             .map_err(drop)?;
 
         unsafe {
-            let heap = HEAP_BUF.init();
+            // Create a heap type from the given storage buffer
+            let heap = HEAP_BUF.take_heap();
 
             // Initialize the Free Queue
             FREE_Q.init();
@@ -54,21 +78,25 @@ impl AHeap {
             (*self.heap.get()).write(heap);
         }
 
+        // We have exclusive access, a "store" is okay.
         self.state.store(Self::INIT_IDLE, Ordering::SeqCst);
 
         Ok(())
     }
 
     pub fn try_lock(&'static self) -> Option<HeapGuard> {
+        // The heap must be idle
         self.state
             .compare_exchange(
                 Self::INIT_IDLE,
-                Self::INIT_BUSY,
+                Self::BUSY_LOCKED,
                 Ordering::SeqCst,
                 Ordering::SeqCst,
             )
             .ok()?;
 
+        // SAFETY: If we were in the INIT_IDLE state, then the heap has been
+        // initialized (is valid), and no other access exists (mutually exclusive).
         unsafe {
             let heap = &mut *self.heap.get().cast();
             Some(HeapGuard { heap })
@@ -77,18 +105,28 @@ impl AHeap {
 }
 
 struct FreeQueue {
+    // NOTE: This is because MpMcQueue has non-zero initialized state, which means
+    // it would reside in .data instead of .bss. This moves initialization to runtime,
+    // and means that no .data initializer is required
     q: UnsafeCell<MaybeUninit<MpMcQueue<FreeBox, FREE_Q_LEN>>>,
 }
 
+// SAFETY: Access to the FreeQueue (a singleton) is mediated by the AHeap.state
+// tracking variable.
 unsafe impl Sync for FreeQueue {}
 
 impl FreeQueue {
+    /// Create a new, uninitialized FreeQueue
     const fn new() -> Self {
         Self {
             q: UnsafeCell::new(MaybeUninit::uninit()),
         }
     }
 
+    /// Initialize the FreeQueue.
+    ///
+    /// SAFETY: This function should only ever be called once (usually in the initialization
+    /// of the AHeap singleton).
     unsafe fn init(&self) {
         let new = MpMcQueue::new();
         self.q
@@ -97,23 +135,36 @@ impl FreeQueue {
             .write(new);
     }
 
+    /// Attempt to access the FreeQueue.
+    ///
+    /// Returns access if the FreeQueue has been initialized, and an Error if
+    /// it has not yet been initialized.
     fn try_get(&self) -> Result<&MpMcQueue<FreeBox, FREE_Q_LEN>, ()> {
         let state = HEAP.state.load(Ordering::SeqCst);
 
         if state == AHeap::UNINIT {
             Err(())
         } else {
+            // SAFETY: The MpMcQueue type is Sync, so mutual exclusion is not required
+            // If the HEAP type has been initialized, so has the FreeQueue singleton,
+            // so access is valid.
             unsafe { Ok((*self.q.get()).assume_init_ref()) }
         }
     }
 }
 
+/// A storage wrapper type for the heap payload.
+///
+/// The wrapper is required to impl the `Sync` trait
 struct HeapStorage {
     data: UnsafeCell<[u8; Self::SIZE_BYTES]>,
 }
 
+// SAFETY: Access is only provided through raw pointers, and is exclusively accessed
+// through AHeap allocation methods.
 unsafe impl Sync for HeapStorage {}
 
+/// An Anachro Heap Box Type
 pub struct HeapBox<T> {
     ptr: *mut T,
 }
@@ -133,6 +184,12 @@ impl<T> DerefMut for HeapBox<T> {
 }
 
 impl<T> HeapBox<T> {
+    /// Create a free_box, with location and layout information necessary
+    /// to free the box.
+    ///
+    /// SAFETY: This function creates aliasing pointers for the allocation. It
+    /// should ONLY be called in the destructor of the HeapBox when deallocation
+    /// is about to occur, and access to the Box will not be allowed again.
     unsafe fn free_box(&mut self) -> FreeBox {
         FreeBox {
             ptr: NonNull::new_unchecked(self.ptr.cast::<u8>()),
@@ -143,12 +200,14 @@ impl<T> HeapBox<T> {
 
 impl<T> Drop for HeapBox<T> {
     fn drop(&mut self) {
+        // Calculate the pointer, size, and layout of this allocation
         let free_box = unsafe { self.free_box() };
 
+        // Attempt to get exclusive access to the heap
         if let Some(mut h) = HEAP.try_lock() {
             // If we can access the heap directly, then immediately free this memory
             unsafe {
-                h.deallocate(free_box.ptr, free_box.layout);
+                h.deref_mut().deallocate(free_box.ptr, free_box.layout);
             }
         } else {
             // If not, try to store the allocation into the free list, and it will be
@@ -161,29 +220,54 @@ impl<T> Drop for HeapBox<T> {
     }
 }
 
+/// A type representing a request to free a given allocation of memory.
 pub struct FreeBox {
     ptr: NonNull<u8>,
     layout: Layout,
 }
 
+/// A guard type that provides mutually exclusive access to the allocator as
+/// long as the guard is held.
 pub struct HeapGuard {
     heap: &'static mut AHeap,
 }
 
+// Public HeapGuard methods
 impl HeapGuard {
+    /// The free space (in bytes) available to the allocator
+    pub fn free_space(&self) -> usize {
+        self.deref().free()
+    }
+
+    /// The used space (in bytes) available to the allocator
+    pub fn used_space(&self) -> usize {
+        self.deref().used()
+    }
+
+    /// Attempt to allocate a HeapBox using the allocator
+    ///
+    /// If space was available, the allocation will be returned. If not, an
+    /// error will be returned
     pub fn alloc_box<T>(&mut self, data: T) -> Result<HeapBox<T>, ()> {
-        // First, free all pending memory
+        // First, grab the Free Queue. Access here only fails if the Queue
+        // was not initialized (should be impossible).
         let free_q = FREE_Q.try_get()?;
+
+        // Then, free all pending memory in order to maximize space available.
         while let Some(FreeBox { ptr, layout }) = free_q.dequeue() {
+            // SAFETY: We have mutually exclusive access to the allocator, and
+            // the pointer and layout are correctly calculated by the relevant
+            // FreeBox types.
             unsafe {
-                self.deallocate(ptr, layout);
+                self.deref_mut().deallocate(ptr, layout);
             }
         }
 
         // Then, attempt to allocate the requested T.
-        let nnu8 = self.allocate_first_fit(Layout::new::<T>())?;
+        let nnu8 = self.deref_mut().allocate_first_fit(Layout::new::<T>())?;
         let ptr = nnu8.as_ptr().cast::<T>();
 
+        // And initialize it with the contents given to us
         unsafe {
             ptr.write(data);
         }
@@ -192,17 +276,18 @@ impl HeapGuard {
     }
 }
 
-impl Deref for HeapGuard {
-    type Target = Heap;
-
-    fn deref(&self) -> &Self::Target {
+// Private HeapGuard methods.
+//
+// NOTE: These are NOT impls of the Deref/DerefMut traits, as I don't actually
+// want those methods to be available to downstream users of the HeapGuard
+// type. For now, I'd like them to only use the "public" allocation interfaces.
+impl HeapGuard {
+    fn deref(&self) -> &Heap {
         // SAFETY: If we have a HeapGuard, we have single access.
         unsafe { (*self.heap.heap.get()).assume_init_ref() }
     }
-}
 
-impl DerefMut for HeapGuard {
-    fn deref_mut(&mut self) -> &mut Self::Target {
+    fn deref_mut(&mut self) -> &mut Heap {
         // SAFETY: If we have a HeapGuard, we have single access.
         unsafe { (*self.heap.heap.get()).assume_init_mut() }
     }
@@ -218,20 +303,26 @@ impl HeapStorage {
     const SIZE_KB: usize = 64;
     const SIZE_BYTES: usize = Self::SIZE_KB * 1024;
 
+    /// Create a new uninitialized storage buffer.
     const fn new() -> Self {
         Self {
             data: UnsafeCell::new([0u8; Self::SIZE_BYTES]),
         }
     }
 
+    /// Obtain the starting address and total size of the storage buffer.
     fn addr_sz(&self) -> (usize, usize) {
         let ptr = self.data.get();
         let addr = ptr as usize;
         (addr, Self::SIZE_BYTES)
     }
 
-    // SAFETY: Only call once!
-    unsafe fn init(&self) -> Heap {
+
+    /// Create a Heap object, using the storage contents as the heap memory range.
+    ///
+    /// SAFETY: This method should only be called once, typically in the
+    /// initialization of the AHeap object.
+    unsafe fn take_heap(&self) -> Heap {
         let mut heap = Heap::empty();
         let (addr, size) = HEAP_BUF.addr_sz();
         heap.init(addr, size);
