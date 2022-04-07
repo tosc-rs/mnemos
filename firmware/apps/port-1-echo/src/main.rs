5aa24a787db57346fa79bd9ad9ae8457d8499ed7
James Munns <james@onevariable.com>
2022-04-07 03:53:47 +0200

    Remove dead code

--- firmware/apps/port-1-echo/src/main.rs
+++ firmware/apps/port-1-echo/src/main.rs
@@ -11,24 +11,10 @@ pub fn entry() -> ! {
     let mut buf = [0u8; 128];
 
     loop {
-        for _ in 0..100 {
-            if let Ok(data) = serial::read_port(0, &mut buf) {
-                match serial::write_port(1, data) {
-                    Ok(None) => {},
-                    Ok(Some(_)) => {},
-                    Err(()) => {},
-                }
-            } else {
-                // defmt::println!("Read port 0 failed!");
-            }
-
-            time::sleep_micros(10_000).ok();
+        if let Ok(data) = serial::read_port(0, &mut buf) {
+            serial::write_port(1, data).ok();
         }
 
-        // if let Some(hg) = HEAP.try_lock() {
-        //     let used = hg.used_space();
-        //     let free = hg.free_space();
-        //     defmt::println!("used: {=usize}, free: {=usize}", used, free);
-        // }
+        time::sleep_micros(10_000).ok();
     }
 }
