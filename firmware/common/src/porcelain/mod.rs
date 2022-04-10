b82f034afa6b0e1ee6d6ea0a809e93f0bcd25be3
James Munns <james@onevariable.com>
2022-04-10 18:59:37 +0200

    Add missing porcelain functions

--- firmware/common/src/porcelain/mod.rs
+++ firmware/common/src/porcelain/mod.rs
@@ -1,3 +1,9 @@
+// TODO: To be honest, these are a bit more "plumbing" than "porcelain".
+//
+// The "real" porcelain should feel more like the stdlib, these are more
+// safe wrappers of -sys functions. At some point move these functions
+// to a "plumbing" module, and add a higher level "porcelain" level
+
 use crate::syscall::{
     request::SysCallRequest,
     success::SysCallSuccess,
@@ -105,8 +111,23 @@ pub mod block_storage {
     use super::*;
     use crate::syscall::{
         request::BlockRequest,
-        success::{BlockSuccess, StoreInfo, BlockInfo},
+        success::{BlockSuccess, StoreInfo, BlockStatus}, BlockKind,
     };
+
+    // TODO: I should probably come up with a policy for how to handle types that
+    // remove the SysCallSlice* types. For example, in other porcelain calls, I just
+    // return slices (instead of SCS* types). This is sort of type duplication, BUT it's
+    // probably also good practice NOT to expose any of the syscall "wire types" to the
+    // end user.
+    //
+    // Something to think about, at least.
+    pub struct BlockInfoStr<'a>{
+        pub length: u32,
+        pub capacity: u32,
+        pub kind: BlockKind,
+        pub status: BlockStatus,
+        pub name: Option<&'a str>,
+    }
 
     pub fn success_filter(succ: SysCallSuccess) -> Result<BlockSuccess, ()> {
         if let SysCallSuccess::BlockStore(bsr) = succ {
@@ -127,7 +148,7 @@ pub mod block_storage {
         }
     }
 
-    pub fn block_info<'a>(block: u32, name_buf: &'a mut [u8]) -> Result<BlockInfo<'a>, ()> {
+    pub fn block_info<'a>(block: u32, name_buf: &'a mut [u8]) -> Result<BlockInfoStr<'a>, ()> {
         let req = SysCallRequest::BlockStore(BlockRequest::BlockInfo {
             block_idx: block,
             name_buf: name_buf.into()
@@ -135,10 +156,84 @@ pub mod block_storage {
         let resp = success_filter(try_syscall(req)?)?;
 
         if let BlockSuccess::BlockInfo(bi) = resp {
-            Ok(bi)
+            Ok(BlockInfoStr {
+                length: bi.length,
+                capacity: bi.capacity,
+                kind: bi.kind,
+                status: bi.status,
+                name: bi.name.and_then(|scs| {
+                    let bytes = unsafe { scs.to_slice() };
+                    // TODO: this *probably* could be unchecked, but for now
+                    // just report no name on an invalid decode
+                    core::str::from_utf8(bytes).ok()
+                }),
+            })
         } else {
             Err(())
         }
     }
 
+    pub fn block_open(block: u32) -> Result<(), ()> {
+        let req = SysCallRequest::BlockStore(BlockRequest::BlockOpen { block_idx: block });
+        let resp = success_filter(try_syscall(req)?)?;
+
+        if let BlockSuccess::BlockOpened = resp {
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    pub fn block_read<'a>(
+        block: u32,
+        offset: u32,
+        dest_buf: &'a mut [u8]
+    ) -> Result<&'a mut [u8], ()> {
+        let req = SysCallRequest::BlockStore(BlockRequest::BlockRead {
+            block_idx: block,
+            offset,
+            dest_buf: dest_buf.into(),
+        });
+        let resp = success_filter(try_syscall(req)?)?;
+
+        if let BlockSuccess::BlockRead { dest_buf } = resp {
+            Ok(unsafe { dest_buf.to_slice_mut() })
+        } else {
+            Err(())
+        }
+    }
+
+    pub fn block_write(block: u32, offset: u32, bytes: &[u8]) -> Result<(), ()> {
+        let req = SysCallRequest::BlockStore(BlockRequest::BlockWrite {
+            block_idx: block,
+            offset,
+            src_buf: bytes.into(),
+        });
+        let resp = success_filter(try_syscall(req)?)?;
+
+        if let BlockSuccess::BlockWritten = resp {
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    pub fn block_close(block: u32, name: &str, len: u32, kind: BlockKind) -> Result<(), ()> {
+        let req = SysCallRequest::BlockStore(BlockRequest::BlockClose {
+            block_idx: block,
+            name: name.as_bytes().into(),
+            len,
+            kind,
+        });
+        let resp = success_filter(try_syscall(req)?)?;
+
+        if let BlockSuccess::BlockClosed = resp {
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    // TODO: I should probably have some kind of `Block` type that does closing and stuff
+    // in a typical `File` kind of way.
 }
