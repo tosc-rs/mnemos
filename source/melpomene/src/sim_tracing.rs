be6d3412cb10cc9b11fb74cf3b6649475a96dd33
James Munns <james@onevariable.com>
2022-07-07 13:48:29 +0200

    Add optional modality tracing subscriber

--- source/melpomene/src/sim_tracing.rs
+++ source/melpomene/src/sim_tracing.rs
@@ -1,5 +1,9 @@
-use tracing_subscriber;
-
+#[cfg(feature = "trace-fmt")]
 pub fn setup_tracing() {
     tracing_subscriber::fmt::init();
+}
+
+#[cfg(feature = "trace-modality")]
+pub fn setup_tracing() {
+    tracing_modality::TracingModality::init().expect("init");
 }
