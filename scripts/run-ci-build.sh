be96fcb58c9fb6a986118221cfcb12221e98004e
James Munns <james@onevariable.com>
2022-08-04 18:40:19 +0200

    Fix relative path

--- scripts/run-ci-build.sh
+++ scripts/run-ci-build.sh
@@ -13,9 +13,10 @@ rm -rf ./target/ci-publish || :
 mkdir -p ./target/ci-publish/
 cp -r ./target/doc ./target/ci-publish/
 
+# note: --dest-dir is relative from the book.toml
 ./target/mdbook \
     build \
-    --dest-dir ./target/ci-publish/book \
+    --dest-dir ./../target/ci-publish/book \
     ./book
 
 cp ./assets/index.html ./target/ci-publish
