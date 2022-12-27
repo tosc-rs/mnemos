f763fb86fa9f20ef8130416b45830131a119bc0a
James Munns <james@onevariable.com>
2022-12-27 18:46:07 +0100

    Manual impl, let's go postcard next

--- src/word.rs
+++ src/word.rs
@@ -1,11 +1,16 @@
 use core::mem::MaybeUninit;
 
+use crate::cfa::ShortWord;
+
 // Use a union so that things work on both 32- and 64-bit systems,
 // so the *data* is always 32 bits, but the pointer is whatever the
 // native word size is.
+#[repr(C)]
+#[derive(Copy, Clone)]
 pub union Word {
     pub data: i32,
     pub ptr: *mut (),
+    pub hdr: ShortWord,
 }
 
 impl Word {
@@ -23,6 +28,15 @@ impl Word {
         let mut mu_word: MaybeUninit<Word> = MaybeUninit::zeroed();
         unsafe {
             mu_word.as_mut_ptr().cast::<*mut T>().write(ptr);
+            mu_word.assume_init()
+        }
+    }
+
+    #[inline]
+    pub fn hdr(hdr: ShortWord) -> Self {
+        let mut mu_word: MaybeUninit<Word> = MaybeUninit::zeroed();
+        unsafe {
+            mu_word.as_mut_ptr().cast::<ShortWord>().write(hdr);
             mu_word.assume_init()
         }
     }
