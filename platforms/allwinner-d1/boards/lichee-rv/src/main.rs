4b3fd83563f1a56b0f8ab0628603eb2edd59e8aa
James Munns <james@onevariable.com>
2023-06-11 16:37:08 +0200

    Basic mainloop

--- platforms/allwinner-d1/boards/lichee-rv/src/main.rs
+++ platforms/allwinner-d1/boards/lichee-rv/src/main.rs
@@ -1,15 +1,51 @@
 #![no_std]
 #![no_main]
 
+use core::{panic::PanicInfo, time::Duration};
+use drivers::Ram;
+use kernel::{Kernel, KernelSettings};
+use embedded_hal::blocking::delay::DelayMs;
+
+const HEAP_SIZE: usize = 384 * 1024 * 1024;
+
+#[link_section = ".aheap.AHEAP"]
+#[used]
+static AHEAP: Ram<HEAP_SIZE> = Ram::new();
+
 #[allow(non_snake_case)]
 #[riscv_rt::entry]
 fn main() -> ! {
-    loop {
+    let k = initialize_kernel().unwrap();
 
+    // k.initialize(async {}).unwrap();
+
+    loop {
+        let tick = k.tick();
+
+        if !tick.has_remaining {
+            let mut delay = riscv::delay::McycleDelay::new(1_080_000_000);
+            delay.delay_ms(1);
+            let _turn = k.timer().force_advance_ticks(1);
+        }
     }
 }
 
-use core::panic::PanicInfo;
+fn initialize_kernel() -> Result<&'static Kernel, ()> {
+    let k_settings = KernelSettings {
+        heap_start: AHEAP.as_ptr(),
+        heap_size: HEAP_SIZE,
+        max_drivers: 16,
+        k2u_size: 4096,
+        u2k_size: 4096,
+        timer_granularity: Duration::from_millis(1),
+    };
+    let k = unsafe {
+        Kernel::new(k_settings).map_err(drop)?.leak().as_ref()
+    };
+    Ok(k)
+}
+
+
 
 #[panic_handler]
 fn handler(_info: &PanicInfo) -> ! {
--- platforms/allwinner-d1/drivers/src/lib.rs
+++ platforms/allwinner-d1/drivers/src/lib.rs
@@ -1,1 +1,22 @@
 #![no_std]
+
+use core::{mem::MaybeUninit, cell::UnsafeCell};
+
+pub struct Ram<const N: usize> {
+    inner: MaybeUninit<UnsafeCell<[u8; N]>>,
+}
+
+unsafe impl<const N: usize> Sync for Ram<N> {}
+impl<const N: usize> Ram<N> {
+    pub const fn new() -> Self {
+        Self {
+            inner: MaybeUninit::uninit(),
+        }
+    }
+
+    pub fn as_ptr(&'static self) -> *mut u8 {
+        let p: *mut UnsafeCell<[u8; N]> = self.inner.as_ptr().cast_mut();
+        let p: *mut u8 = p.cast();
+        p
+    }
+}
