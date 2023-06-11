b1d97abf84967d65171eeaf83eb2cafbd48d60a0
James Munns <james@onevariable.com>
2023-06-11 16:12:03 +0200

    Empty skeleton

--- platforms/allwinner-d1/boards/lichee-rv/src/main.rs
+++ platforms/allwinner-d1/boards/lichee-rv/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[allow(non_snake_case)]
+#[riscv_rt::entry]
+fn main() -> ! {
+    loop {
+
+    }
+}
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn handler(_info: &PanicInfo) -> ! {
+    loop {
+        core::sync::atomic::fence(core::sync::atomic::Ordering::SeqCst);
+    }
 }
