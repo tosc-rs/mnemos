6ede6fc85d05196b858778a300bf6afcc97d4979
Eliza Weisman <eliza@buoyant.io>
2023-06-22 14:22:55 -0700

    prettier

--- tools/crowtty/src/trace.rs
+++ tools/crowtty/src/trace.rs
@@ -1,7 +1,9 @@
 use postcard::accumulator::{CobsAccumulator, FeedResult};
-use std::{time::Instant, sync::mpsc, fmt::Write, collections::HashMap, num::NonZeroU64};
-use tracing_serde_structured::{SerializeRecordFields, SerializeSpanFields, SerializeValue, CowString};
+use std::{time::Instant, sync::mpsc, fmt::{self, Write}, collections::HashMap, num::NonZeroU64};
+use tracing_serde_structured::{SerializeRecordFields, SerializeSpanFields, SerializeValue, CowString, SerializeLevel};
 use mnemos_trace_proto::TraceEvent;
+
+use owo_colors::{OwoColorize, Stream};
 use crate::LogTag;
 
 pub(crate) fn decode(rx: mpsc::Receiver<Vec<u8>>, tag: LogTag) {
@@ -51,22 +53,26 @@ struct Span {
 impl TraceState {
     fn write_span_cx(&mut self) {
         let spans = self.stack.iter().filter_map(|id| self.spans.get(id));
+        let mut any = false;
         for span in spans {
             self.textbuf.push_str(span.repr.as_str());
             self.textbuf.push(':');
+            any = true;
+        }
+        if any {
+            self.textbuf.push(' ');
         }
     }
 
     fn event(&mut self, ev: TraceEvent<'_>) {
         let now = Instant::now();
-        let elapsed = now - self.trace_start;
         match ev {
             TraceEvent::Event(ev) => {
                 let target = ev.metadata.target.as_str();
-                let level = ev.metadata.level;
-                write!(&mut self.textbuf, "{} {level:<5?} ", self.tag).unwrap();
+                let target = target.if_supports_color(Stream::Stdout, |target| target.italic());
+                let level = DisplayLevel(ev.metadata.level);
+                write!(&mut self.textbuf, "{} {level} {target}: ", self.tag).unwrap();
                 self.write_span_cx();
-                write!(&mut self.textbuf, "{target}: ").unwrap();
                 let SerializeRecordFields::De(ref fields) = ev.fields else {
                     unreachable!("we are deserializing!");
                 };
@@ -76,17 +82,19 @@ impl TraceState {
             }
             TraceEvent::NewSpan { id, attributes} => {
                 let mut repr = String::new();
-                repr.push_str(attributes.metadata.name.as_str());
-                repr.push('{');
+                let name = attributes.metadata.name.as_str();
+                write!(repr, "{}", format_args!("{name}{{").if_supports_color(Stream::Stdout, |x| x.bold())).unwrap();
                 let SerializeSpanFields::De(ref fields) = attributes.fields else {
                     unreachable!("we are deserializing!");
                 };
                 write_fields(&mut repr, fields);
-                repr.push('}');
-
-                let level = attributes.metadata.level;
+                write!(repr, "{}", "}".if_supports_color(Stream::Stderr, |x| x.bold())).unwrap();
+                
+                let level = DisplayLevel(attributes.metadata.level);
                 let target = attributes.metadata.target.as_str();
-                write!(&mut self.textbuf, "{} {level:<5?} ", self.tag).unwrap();
+                let target = target.if_supports_color(Stream::Stdout, |target| target.italic());
+
+                write!(&mut self.textbuf, "{} {level} ", self.tag).unwrap();
                 self.write_span_cx();
                 write!(&mut self.textbuf, "-> {target}::{repr}").unwrap();
                 println!("{}", self.textbuf);
@@ -114,7 +122,7 @@ fn write_fields<'a>(to: &mut String, fields: impl IntoIterator<Item = (&'a CowSt
     if let Some((key, val)) = fields.next() {
         write_kv(key, val, to);
         for (key, val) in fields {
-            to.push_str(", ");
+            write!(to, "{}", ", ".if_supports_color(Stream::Stdout, |delim| delim.dimmed())).unwrap();
             write_kv(key, val, to);
         }
     }
@@ -124,8 +132,8 @@ fn write_kv(key: &CowString<'_>, val: &SerializeValue<'_>, to: &mut String) {
     use tracing_serde_structured::DebugRecord;
 
     let key = key.as_str();
-    to.push_str(key);
-    to.push('=');
+    let key = key.if_supports_color(Stream::Stdout, |k| k.bold());
+    write!(to, "{key}{}", "=".if_supports_color(Stream::Stdout, |delim| delim.dimmed())).unwrap();
 
     match val {
         SerializeValue::Debug(DebugRecord::De(d)) => to.push_str(d.as_str()),
@@ -136,5 +144,19 @@ fn write_kv(key: &CowString<'_>, val: &SerializeValue<'_>, to: &mut String) {
         SerializeValue::U64(x) => write!(to, "{x}").unwrap(),
         SerializeValue::Bool(x)  => write!(to, "{x}").unwrap(),
         _ => to.push_str("???"),
+    }
+}
+
+struct DisplayLevel(SerializeLevel);
+
+impl fmt::Display for DisplayLevel {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.0 {
+            SerializeLevel::Trace => write!(f, "{}", "TRCE".if_supports_color(Stream::Stdout, |l| l.purple())),
+            SerializeLevel::Debug => write!(f, "{}", "DBUG".if_supports_color(Stream::Stdout, |l| l.blue())),
+            SerializeLevel::Info => write!(f, "{}", "INFO".if_supports_color(Stream::Stdout, |l| l.green())),
+            SerializeLevel::Warn => write!(f, "{}", "WARN".if_supports_color(Stream::Stdout, |l| l.yellow())),
+            SerializeLevel::Error => write!(f, "{}", "ERR!".if_supports_color(Stream::Stdout, |l| l.red())),
+        }
     }
 }
