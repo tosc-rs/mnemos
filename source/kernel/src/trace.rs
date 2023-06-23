c62e60d203191053228dc923c843781efa6bec9c
Eliza Weisman <eliza@buoyant.io>
2023-06-23 09:41:11 -0700

    refac(trace): DRY up trace collector

--- source/kernel/src/trace.rs
+++ source/kernel/src/trace.rs
@@ -2,7 +2,7 @@ use crate::{comms::bbq, drivers::serial_mux};
 use level_filters::LevelFilter;
 use mnemos_trace_proto::TraceEvent;
 use mycelium_util::sync::InitOnce;
-use portable_atomic::{AtomicPtr, AtomicU64, Ordering};
+use portable_atomic::{AtomicPtr, AtomicU64, AtomicUsize, Ordering};
 
 pub use tracing_02::*;
 use tracing_core_02::span::Current;
@@ -10,6 +10,7 @@ use tracing_serde_structured::{AsSerde, SerializeRecordFields, SerializeSpanFiel
 
 pub struct SerialCollector {
     tx: InitOnce<bbq::SpscProducer>,
+
     /// ID of the current span.
     ///
     /// **Note**: This collector only works correctly on single-threaded hardware!
@@ -18,6 +19,11 @@ pub struct SerialCollector {
 
     /// ID of the next span.
     next_id: AtomicU64,
+
+    /// Counter of events that were dropped due to insufficient buffer capacity.
+    ///
+    // TODO(eliza): Currently, this is recorded but not actually consumed...
+    dropped_events: AtomicUsize,
 
     max_level: LevelFilter,
 }
@@ -34,6 +40,7 @@ impl SerialCollector {
             current_span: AtomicU64::new(0),
             current_meta: AtomicPtr::new(core::ptr::null_mut()),
             next_id: AtomicU64::new(1),
+            dropped_events: AtomicUsize::new(0),
             max_level,
         }
     }
@@ -54,6 +61,32 @@ impl SerialCollector {
             .expect("cannot set global default tracing dispatcher");
     }
 
+    /// Serialize a `TraceEvent`, returning `true` if the event was correctly serialized.
+    fn send_event<'a>(&self, sz: usize, event: impl FnOnce() -> TraceEvent<'a>) -> bool {
+        let Some(mut wgr) = self.tx.get().send_grant_exact_sync(sz) else {
+            self.dropped_events.fetch_add(1, Ordering::Relaxed);
+            return false;
+        };
+
+        // got a write grant! generate the event payload.
+        let ev = event();
+
+        // encode the event to our write grant, and commit however many bytes
+        // were written. if encoding fails, commit 0 bytes, so the region we got
+        // a write grant for can be reused.
+        let len = match postcard::to_slice_cobs(&ev, &mut wgr[..]) {
+            Ok(encoded) => encoded.len(),
+            Err(_) => {
+                self.dropped_events.fetch_add(1, Ordering::Relaxed);
+                0
+            },
+        };
+        wgr.commit(len);
+
+        // return true if we committed a non-zero number of bytes.
+        len > 0
+    }
+
     async fn worker(rx: bbq::Consumer, port: serial_mux::PortHandle) {
         loop {
             let rgr = rx.read_grant().await;
@@ -72,28 +105,25 @@ impl Collect for SerialCollector {
 
     fn register_callsite(&self, metadata: &'static Metadata<'static>) -> tracing_core_02::Interest {
         if !self.enabled(metadata) {
-            return tracing_core_02::collect::Interest::never();
+            return tracing_core_02::Interest::never();
         }
 
         let id = metadata.callsite();
 
         // TODO(eliza): if we can't write a metadata, that's bad news...
-        if let Some(mut wgr) = self.tx.get().send_grant_exact_sync(1024) {
-            let ev = TraceEvent::RegisterMeta {
+        let sent = self.send_event(1024, ||
+            TraceEvent::RegisterMeta {
                 id: mnemos_trace_proto::MetaId::from(id),
                 meta: metadata.as_serde(),
-            };
-            let len = match postcard::to_slice_cobs(&ev, &mut wgr[..]) {
-                Ok(encoded) => encoded.len(),
-                Err(_) => 0, // we will release any bytes written to be clobbered.
-            };
-            wgr.commit(len);
-            if len > 0 {
-                return tracing_core_02::collect::Interest::always();
-            }
-        }
+            });
 
-        tracing_core_02::collect::Interest::never()
+        if sent {
+            tracing_core_02::Interest::always()
+        } else {
+            // if we couldn't send the metadata, skip this callsite, because the
+            // consumer will not be able to understand it without its metadata.
+            tracing_core_02::Interest::never()
+        }
     }
 
     fn max_level_hint(&self) -> Option<LevelFilter> {
@@ -105,71 +135,40 @@ impl Collect for SerialCollector {
             let id = self.next_id.fetch_add(1, Ordering::Relaxed);
             span::Id::from_u64(id)
         };
-        if let Some(mut wgr) = self.tx.get().send_grant_exact_sync(1024) {
-            let ev = TraceEvent::NewSpan {
-                id: id.as_serde(),
-                meta: span.metadata().callsite().into(),
-                parent: span.parent().map(AsSerde::as_serde),
-                is_root: span.is_root(),
-                fields: SerializeSpanFields::Ser(span.values()),
-            };
-            let len = match postcard::to_slice_cobs(&ev, &mut wgr[..]) {
-                Ok(encoded) => encoded.len(),
-                Err(_) => 0, // we will release any bytes written to be clobbered.
-            };
-            wgr.commit(len);
-        }
+
+        self.send_event(1024, || TraceEvent::NewSpan {
+            id: id.as_serde(),
+            meta: span.metadata().callsite().into(),
+            parent: span.parent().map(AsSerde::as_serde),
+            is_root: span.is_root(),
+            fields: SerializeSpanFields::Ser(span.values()),
+        });
+
         id
     }
 
-    fn record(&self, span: &span::Id, values: &span::Record<'_>) {
+    fn record(&self, _: &span::Id, _: &span::Record<'_>) {
         // todo!("eliza")
     }
 
     fn enter(&self, span: &span::Id) {
-        if let Some(mut wgr) = self.tx.get().send_grant_exact_sync(16) {
-            let len =
-                match postcard::to_slice_cobs(&TraceEvent::Enter(span.as_serde()), &mut wgr[..]) {
-                    Ok(encoded) => encoded.len(),
-                    Err(_) => 0, // we will release any bytes written to be clobbered.
-                };
-            wgr.commit(len);
-        }
+        self.send_event(16, || TraceEvent::Enter(span.as_serde()));
     }
 
     fn exit(&self, span: &span::Id) {
-        if let Some(mut wgr) = self.tx.get().send_grant_exact_sync(16) {
-            let len =
-                match postcard::to_slice_cobs(&TraceEvent::Exit(span.as_serde()), &mut wgr[..]) {
-                    Ok(encoded) => encoded.len(),
-                    Err(_) => 0, // we will release any bytes written to be clobbered.
-                };
-            wgr.commit(len);
-        }
+        self.send_event(16, || TraceEvent::Exit(span.as_serde()));
     }
 
-    fn record_follows_from(&self, span: &span::Id, follows: &span::Id) {
+    fn record_follows_from(&self, _: &span::Id, _: &span::Id) {
         // nop for now
     }
 
     fn event(&self, event: &Event<'_>) {
-        if let Some(mut wgr) = self.tx.get().send_grant_exact_sync(1024) {
-            let ev = TraceEvent::Event {
-                meta: event.metadata().callsite().into(),
-                fields: SerializeRecordFields::Ser(event),
-                parent: event.parent().map(AsSerde::as_serde),
-            };
-            let len = match postcard::to_slice_cobs(&ev, &mut wgr[..]) {
-                Ok(encoded) => encoded.len(),
-                Err(_) => 0, // we will release any bytes written to be clobbered.
-            };
-            wgr.commit(len);
-        } else {
-            // drop the new event
-            // XXX(eliza): it would be nicer to get ring-buffer behavior
-            // (dropping the oldest, rather than the newest, entry)...we
-            // probably need a new channel type for that.
-        }
+        self.send_event(1024, || TraceEvent::Event {
+            meta: event.metadata().callsite().into(),
+            fields: SerializeRecordFields::Ser(event),
+            parent: event.parent().map(AsSerde::as_serde),
+        });
     }
 
     fn current_span(&self) -> Current {
@@ -188,29 +187,12 @@ impl Collect for SerialCollector {
     }
 
     fn clone_span(&self, span: &span::Id) -> span::Id {
-        if let Some(mut wgr) = self.tx.get().send_grant_exact_sync(16) {
-            let len = match postcard::to_slice_cobs(
-                &TraceEvent::CloneSpan(span.as_serde()),
-                &mut wgr[..],
-            ) {
-                Ok(encoded) => encoded.len(),
-                Err(_) => 0, // we will release any bytes written to be clobbered.
-            };
-            wgr.commit(len);
-        }
+        self.send_event(16, || TraceEvent::CloneSpan(span.as_serde()));
         span.clone()
     }
 
     fn try_close(&self, span: span::Id) -> bool {
-        if let Some(mut wgr) = self.tx.get().send_grant_exact_sync(16) {
-            let len =
-                match postcard::to_slice_cobs(&TraceEvent::DropSpan(span.as_serde()), &mut wgr[..])
-                {
-                    Ok(encoded) => encoded.len(),
-                    Err(_) => 0, // we will release any bytes written to be clobbered.
-                };
-            wgr.commit(len);
-        }
+        self.send_event(16, || TraceEvent::DropSpan(span.as_serde()));
         false
     }
 }
