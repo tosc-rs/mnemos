d6ec18499e1a14eebc99b8090fef1641993988f8
James Munns <james@onevariable.com>
2023-08-01 13:45:16 +0200

    Switch to curlbash for oranda to save CI time

--- scripts/install-ci-deps.sh
+++ scripts/install-ci-deps.sh
@@ -9,7 +9,9 @@ mkdir -p ./target
 rustup toolchain install stable
 
 # Install Oranda
-# TODO: switch to curlbash once they release a prerelease binary!
-cargo install oranda -f \
-    --git https://github.com/axodotdev/oranda \
-    --rev ec4b0b8360b0adc1aa5240e213bf275b262670e2
+curl \
+    --proto '=https' \
+    --tlsv1.2 \
+    -LsSf \
+    https://github.com/axodotdev/oranda/releases/download/v0.3.0-prerelease.4/oranda-installer.sh \
+    | sh
