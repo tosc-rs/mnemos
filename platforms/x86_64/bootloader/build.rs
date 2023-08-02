524af85cf16cb142423b046c25ea586c129f573e
Eliza Weisman <eliza@buoyant.io>
2023-08-02 16:48:11 -0700

    path engoodening

--- platforms/x86_64/bootloader/build.rs
+++ platforms/x86_64/bootloader/build.rs
@@ -7,14 +7,13 @@ fn main() {
     // https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#artifact-dependencies
     let kernel = PathBuf::from(std::env::var_os("CARGO_BIN_FILE_MNEMOS_bootloader").unwrap());
 
-    // create an UEFI disk image (optional)
-    let uefi_path = out_dir.join("uefi.img");
+    let uefi_path = out_dir.join("mnemos-x86_64-uefi.img");
     bootloader::UefiBoot::new(&kernel)
         .create_disk_image(&uefi_path)
         .unwrap();
 
     // create a BIOS disk image
-    let bios_path = out_dir.join("bios.img");
+    let bios_path = out_dir.join("mnemos-x86_64-bios.img");
     bootloader::BiosBoot::new(&kernel)
         .create_disk_image(&bios_path)
         .unwrap();
