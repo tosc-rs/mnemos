83487ffdca84dca2179958cc98cb1f7d071b52a3
James Munns <james@onevariable.com>
2023-08-03 03:52:01 +0200

    Split out per-target tomls

--- platforms/allwinner-d1/src/main.rs
+++ platforms/allwinner-d1/src/main.rs
@@ -4,8 +4,9 @@
 extern crate alloc;
 
 use core::time::Duration;
-use d1_config::{PlatformConfig, Mapping};
+use d1_config::{PlatformConfig, Mapping, InterruptPin, LedBlinkPin};
 use kernel::maitake::sync::WaitCell;
+use mnemos_beepy::i2c_puppet::{I2cPuppetSettings, I2cPuppetServer, HsvColor, I2cPuppetClient};
 use mnemos_d1::{
     ccu::Ccu,
     dmac::Dmac,
@@ -49,126 +50,151 @@ fn main() -> ! {
     };
     let timers = Timers::new(p.TIMER);
     let dmac = Dmac::new(p.DMAC, &mut ccu);
-    let plic = Plic::new(p.PLIC);
+    let mut plic = Plic::new(p.PLIC);
 
-//     let i2c_puppet_irq = init_i2c_puppet_irq(&mut p.GPIO, &mut plic);
+    let puppet_enabled = i2c0.is_some() && config.platform.i2c_puppet.enabled;
 
-    let d1 = D1::initialize(timers, uart, spim, dmac, plic, i2c0);
+    let i2c_puppet_irq = if puppet_enabled {
+        // This is the only pin we support
+        let InterruptPin::PB7 = config.platform.i2c_puppet.interrupt_pin;
+        Some(init_i2c_puppet_irq_pb7(&mut p.GPIO, &mut plic))
+    } else {
+        None
+    };
 
-//     p.GPIO.pd_cfg2.modify(|_r, w| {
-//         w.pd18_select().output();
-//         w
-//     });
-//     p.GPIO.pd_dat.modify(|_r, w| {
-//         w.pd_dat().variant(1 << 18);
-//         w
-//     });
+    let d1 = D1::initialize(
+        timers,
+        uart,
+        spim,
+        dmac,
+        plic,
+        i2c0,
+        config.kernel,
+        config.services,
+    );
 
-    p.GPIO.pc_cfg0.modify(|_r, w| {
-        w.pc1_select().output();
-        w
-    });
-    p.GPIO.pc_dat.modify(|_r, w| {
-        w.pc_dat().variant(0b0000_0010);
-        w
-    });
-
-//     // Initialize LED loop
-//     d1.kernel
-//         .initialize(async move {
-//             loop {
-//                 p.GPIO.pd_dat.modify(|_r, w| {
-//                     w.pd_dat().variant(1 << 18);
-//                     w
-//                 });
-//                 d1.kernel.sleep(Duration::from_millis(250)).await;
-//                 p.GPIO.pd_dat.modify(|_r, w| {
-//                     w.pd_dat().variant(0);
-//                     w
-//                 });
-//                 d1.kernel.sleep(Duration::from_millis(250)).await;
-//             }
-//         })
-//         .unwrap();
-
-    // Initialize LED loop
-    d1.kernel
-        .initialize(async move {
-            loop {
+    if config.platform.blink_service.enabled {
+        match config.platform.blink_service.blink_pin {
+            LedBlinkPin::PC1 => {
+                p.GPIO.pc_cfg0.modify(|_r, w| {
+                    w.pc1_select().output();
+                    w
+                });
                 p.GPIO.pc_dat.modify(|_r, w| {
                     w.pc_dat().variant(0b0000_0010);
                     w
                 });
-                d1.kernel.sleep(Duration::from_millis(250)).await;
-                p.GPIO.pc_dat.modify(|_r, w| {
-                    w.pc_dat().variant(0b0000_0000);
+
+            // Initialize LED loop
+            d1.kernel
+                .initialize(async move {
+                    loop {
+                        p.GPIO.pc_dat.modify(|_r, w| {
+                            w.pc_dat().variant(0b0000_0010);
+                            w
+                        });
+                        d1.kernel.sleep(Duration::from_millis(250)).await;
+                        p.GPIO.pc_dat.modify(|_r, w| {
+                            w.pc_dat().variant(0b0000_0000);
+                            w
+                        });
+                        d1.kernel.sleep(Duration::from_millis(250)).await;
+                    }
+                })
+                .unwrap();
+            },
+            LedBlinkPin::PD18 => {
+                p.GPIO.pd_cfg2.modify(|_r, w| {
+                    w.pd18_select().output();
                     w
                 });
-                d1.kernel.sleep(Duration::from_millis(250)).await;
-            }
-        })
-        .unwrap();
+                p.GPIO.pd_dat.modify(|_r, w| {
+                    w.pd_dat().variant(1 << 18);
+                    w
+                });
+
+                // Initialize LED loop
+                d1.kernel
+                    .initialize(async move {
+                        loop {
+                            p.GPIO.pd_dat.modify(|_r, w| {
+                                w.pd_dat().variant(1 << 18);
+                                w
+                            });
+                            d1.kernel.sleep(Duration::from_millis(250)).await;
+                            p.GPIO.pd_dat.modify(|_r, w| {
+                                w.pd_dat().variant(0);
+                                w
+                            });
+                            d1.kernel.sleep(Duration::from_millis(250)).await;
+                        }
+                    })
+                    .unwrap();
+            },
+        }
+    }
 
     d1.initialize_sharp_display();
 
-//     let i2c_puppet_up = d1
-//         .kernel
-//         .initialize(async move {
-//             let settings = I2cPuppetSettings::default().with_poll_interval(Duration::from_secs(2));
-//             d1.kernel.sleep(Duration::from_secs(2)).await;
-//             I2cPuppetServer::register(d1.kernel, settings, i2c_puppet_irq)
-//                 .await
-//                 .expect("failed to register i2c_puppet driver!");
-//         })
-//         .unwrap();
+    if let Some(puppet_irq) = i2c_puppet_irq {
+        let i2c_puppet_up = d1
+            .kernel
+            .initialize(async move {
+                let settings = I2cPuppetSettings::default().with_poll_interval(Duration::from_secs(2));
+                d1.kernel.sleep(Duration::from_secs(2)).await;
+                I2cPuppetServer::register(d1.kernel, settings, Some(puppet_irq))
+                    .await
+                    .expect("failed to register i2c_puppet driver!");
+            })
+            .unwrap();
 
-//     d1.kernel
-//         .initialize(async move {
-//             // i2c_puppet demo: print each keypress to the console.
-//             i2c_puppet_up.await.unwrap();
-//             let mut i2c_puppet = I2cPuppetClient::from_registry(d1.kernel).await;
-//             tracing::info!("got i2c puppet client");
+        d1.kernel
+            .initialize(async move {
+                // i2c_puppet demo: print each keypress to the console.
+                i2c_puppet_up.await.unwrap();
+                let mut i2c_puppet = I2cPuppetClient::from_registry(d1.kernel).await;
+                tracing::info!("got i2c puppet client");
 
-//             let mut keys = i2c_puppet
-//                 .subscribe_to_raw_keys()
-//                 .await
-//                 .expect("can't get keys");
-//             tracing::info!("got key subscription");
-//             while let Ok(key) = keys.next_char().await {
-//                 tracing::info!(?key, "got keypress");
-//             }
-//         })
-//         .unwrap();
+                let mut keys = i2c_puppet
+                    .subscribe_to_raw_keys()
+                    .await
+                    .expect("can't get keys");
+                tracing::info!("got key subscription");
+                while let Ok(key) = keys.next_char().await {
+                    tracing::info!(?key, "got keypress");
+                }
+            })
+            .unwrap();
 
 
-//     d1.kernel
-//         .initialize(async move {
-//             // walk through the HSV color space. maybe eventually we'll use the RGB
-//             // LED to display useful information, but this is fun for now.
-//             let mut hue = 0;
+        d1.kernel
+            .initialize(async move {
+                // walk through the HSV color space. maybe eventually we'll use the RGB
+                // LED to display useful information, but this is fun for now.
+                let mut hue = 0;
 
-//             let mut i2c_puppet = I2cPuppetClient::from_registry(d1.kernel).await;
+                let mut i2c_puppet = I2cPuppetClient::from_registry(d1.kernel).await;
 
-//             i2c_puppet
-//                 .toggle_led(true)
-//                 .await
-//                 .expect("can't turn on LED");
-//             loop {
-//                 i2c_puppet
-//                     .set_led_color(HsvColor::from_hue(hue))
-//                     .await
-//                     .expect("can't set color");
-//                 hue = hue.wrapping_add(1);
-//                 d1.kernel.sleep(Duration::from_millis(50)).await;
-//             }
-//         })
-//         .unwrap();
+                i2c_puppet
+                    .toggle_led(true)
+                    .await
+                    .expect("can't turn on LED");
+                loop {
+                    i2c_puppet
+                        .set_led_color(HsvColor::from_hue(hue))
+                        .await
+                        .expect("can't set color");
+                    hue = hue.wrapping_add(1);
+                    d1.kernel.sleep(Duration::from_millis(50)).await;
+                }
+            })
+            .unwrap();
+    }
 
     d1.run()
 }
 
-#[allow(dead_code)]
-fn init_i2c_puppet_irq(gpio: &mut d1_pac::GPIO, plic: &mut Plic) -> &'static WaitCell {
+fn init_i2c_puppet_irq_pb7(gpio: &mut d1_pac::GPIO, plic: &mut Plic) -> &'static WaitCell {
     use d1_pac::Interrupt;
     use mnemos_d1::plic::Priority;
 
