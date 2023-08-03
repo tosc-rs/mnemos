abfe35fe3a2b53be2eec8c6425b8b2f1ec9b50af
Eliza Weisman <eliza@buoyant.io>
2023-08-03 09:52:22 -0700

    nicer CLI for the bootloader qemu runner

--- platforms/x86_64/bootloader/src/main.rs
+++ platforms/x86_64/bootloader/src/main.rs
@@ -1,24 +1,85 @@
+use clap::Parser;
+use std::{fmt, path::PathBuf};
+
+/// Boots a MnemOS x86_64 kernel using QEMU.
+#[derive(Parser, Debug)]
+struct Args {
+    /// Path to the UEFI disk image.
+    ///
+    /// This environment variable is set by the build script and typically does
+    /// not need to be set manually.
+    #[clap(long, env = "UEFI_PATH")]
+    uefi_path: PathBuf,
+
+    /// Path to the BIOS disk image.
+    ///
+    /// This environment variable is set by the build script and typically does
+    /// not need to be set manually.
+    #[clap(long, env = "BIOS_PATH")]
+    bios_path: PathBuf,
+
+    /// Path to the QEMU x86_64 executable.
+    ///
+    /// Generally, this does not need to be overridden, unless the QEMU binary
+    /// has a non-standard name or is not on the PATH.
+    #[clap(long, default_value = "qemu-system-x86_64")]
+    qemu: PathBuf,
+
+    /// Whether to boot using UEFI or BIOS.
+    #[clap(long, default_value_t = BootMode::Uefi)]
+    boot: BootMode,
+
+    /// Extra arguments passed directly to the QEMU command.
+    #[arg(last = true)]
+    qemu_args: Vec<String>,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, clap::ValueEnum)]
+enum BootMode {
+    Uefi,
+    Bios,
+}
+
 fn main() {
-    // read env variables that were set in build script
-    let uefi_path = env!("UEFI_PATH");
-    let bios_path = env!("BIOS_PATH");
+    let Args {
+        uefi_path,
+        bios_path,
+        boot,
+        qemu,
+        qemu_args,
+    } = Args::parse();
 
-    // choose whether to start the UEFI or BIOS image
-    let uefi = true;
+    let mut cmd = std::process::Command::new(qemu);
+    match boot {
+        BootMode::Uefi => {
+            let uefi_path = uefi_path.display();
+            println!("booting using UEFI: {uefi_path}");
 
-    let mut cmd = std::process::Command::new("qemu-system-x86_64");
-    if uefi {
-        cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
-        cmd.arg("-drive")
-            .arg(format!("format=raw,file={uefi_path}"));
+            cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
+            cmd.arg("-drive")
+                .arg(format!("format=raw,file={uefi_path}"));
+        }
+        BootMode::Bios => {
+            let bios_path = bios_path.display();
+            println!("booting using BIOS: {bios_path}");
+            cmd.arg("-drive")
+                .arg(format!("format=raw,file={bios_path}"));
+        }
+    }
 
-        println!("running UEFI: {uefi_path}");
-    } else {
-        cmd.arg("-drive")
-            .arg(format!("format=raw,file={bios_path}"));
-        println!("running BIOS: {bios_path}");
+    if !qemu_args.is_empty() {
+        cmd.args(&qemu_args);
     }
 
     let mut child = cmd.spawn().unwrap();
     child.wait().unwrap();
+}
+
+impl fmt::Display for BootMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Uefi => f.write_str("uefi"),
+            Self::Bios => f.write_str("bios"),
+        }
+    }
 }
