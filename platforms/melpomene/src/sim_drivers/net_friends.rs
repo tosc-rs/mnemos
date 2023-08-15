b8b9d2db6e6baf632e45f84a9dc92f4ad56e2db0
James Munns <james@onevariable.com>
2023-08-15 15:59:18 -0500

    Basic framing

--- platforms/melpomene/src/sim_drivers/net_friends.rs
+++ platforms/melpomene/src/sim_drivers/net_friends.rs
@@ -29,10 +29,13 @@ impl FakeSerialFriend {
 async fn friend_talker(client_stream: TcpStream) -> Result<(), ()> {
     let (mut rx, mut tx) = client_stream.into_split();
     let mut buf = [0u8; 4096];
+    let mut acc = Accumulator {
+        buffer: vec![],
+    };
     loop {
         tokio::time::sleep(Duration::from_secs(1)).await;
         tx.writable().await.map_err(drop)?;
-        tx.try_write(b"hello!").map_err(drop)?;
+        tx.try_write(&encode(b"hello!")).map_err(drop)?;
 
         tokio::time::sleep(Duration::from_millis(100)).await;
 
@@ -40,8 +43,18 @@ async fn friend_talker(client_stream: TcpStream) -> Result<(), ()> {
         match rx.try_read(&mut buf) {
             Ok(0) => todo!(),
             Ok(n) => {
-                let msg = core::str::from_utf8(&buf[..n]).unwrap();
-                tracing::info!("Client got: {msg}");
+                let mut window = &buf[..n];
+                'winny: while !window.is_empty() {
+                    match acc.feed(window) {
+                        AccOut::MessageRem { msg, rem } => {
+                            let msg = core::str::from_utf8(&msg).unwrap();
+                            tracing::info!("Decoded '{msg}'");
+                            window = rem;
+                        },
+                        AccOut::ErrorRem { rem } => todo!(),
+                        AccOut::Consumed => break 'winny,
+                    }
+                }
             },
             Err(e) => todo!("what {e:?}"),
         }
@@ -78,4 +91,44 @@ async fn serial_friend(host_stream: TcpStream) -> Result<(), ()> {
     }
 
     Err(())
+}
+
+struct Accumulator {
+    buffer: Vec<u8>,
+}
+
+enum AccOut<'a> {
+    MessageRem {
+        msg: Vec<u8>,
+        rem: &'a [u8],
+    },
+    ErrorRem {
+        rem: &'a [u8],
+    },
+    Consumed,
+}
+
+fn encode(src: &[u8]) -> Vec<u8> {
+    cobs::encode_vec(src)
+}
+
+impl Accumulator {
+    fn feed<'a>(&mut self, buf: &'a [u8]) -> AccOut<'a> {
+        match buf.split_inclusive(|n| *n == 0).next() {
+            Some(rel) => {
+                let rel_len = rel.len();
+                self.buffer.extend_from_slice(rel);
+                let out = cobs::decode_vec(&self.buffer);
+                self.buffer.clear();
+                match out {
+                    Ok(v) => AccOut::MessageRem { msg: v, rem: &buf[rel_len..] },
+                    Err(_) => AccOut::ErrorRem { rem: &buf[rel_len..] },
+                }
+            },
+            None => {
+                self.buffer.extend_from_slice(buf);
+                AccOut::Consumed
+            },
+        }
+    }
 }
