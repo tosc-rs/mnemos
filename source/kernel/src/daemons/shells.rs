3277997a72c04ae47f9261cfe81c782d257b2591
Anatol Ulrich <e+github@mail.taugt.net>
2023-08-29 22:14:44 +0200

    cleanup

--- source/kernel/src/daemons/shells.rs
+++ source/kernel/src/daemons/shells.rs
@@ -15,7 +15,7 @@ use embedded_graphics::{
 use futures::FutureExt;
 use input_mgr::RingLine;
 use profont::PROFONT_12_POINT;
-use tracing::{debug, error};
+use tracing::error;
 
 use crate::{
     comms::bbq::BidiHandle,
@@ -225,10 +225,10 @@ pub async fn graphical_shell_mono(k: &'static Kernel, settings: GraphicalShellSe
 
     let mut any = true;
 
+    // TODO: https://github.com/tosc-rs/mnemos/issues/255
     let interval = Duration::from_secs(1) / 20;
 
     loop {
-        // debug!("mono shell loop");
         if any {
             ring_drawer::drawer_bw(&mut fc_0, &rline, style.clone()).unwrap();
             match disp_hdl.draw_mono(fc_0).await {
