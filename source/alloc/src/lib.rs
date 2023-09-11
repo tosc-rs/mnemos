aa3a9ad4f39265e1e4e5754df131b24007144d0a
Eliza Weisman <eliza@buoyant.io>
2023-09-11 10:07:56 -0700

    feat(alloc): collect heap statistics
    This branch adds support for collecting heap allocation statistics to
    `mnemos-alloc`. A new "stats" feature flag controls whether or not this
    data is collected.

--- source/alloc/src/lib.rs
+++ source/alloc/src/lib.rs
@@ -5,6 +5,7 @@
 //! types that are intended for use in mnemos' kernel and services.
 
 #![cfg_attr(not(feature = "use-std"), no_std)]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg, doc_cfg_hide))]
 
 pub mod containers;
 pub mod heap;
