edacf6d9fe54e8c7d7dacd48b9499c55bb792e98
Eliza Weisman <eliza@buoyant.io>
2023-09-16 09:03:43 -0700

    platform(D1): features for i2c_puppet/SHARP display (#274)

--- platforms/allwinner-d1/src/bin/mq-pro.rs
+++ platforms/allwinner-d1/src/bin/mq-pro.rs
@@ -4,11 +4,6 @@
 #[allow(non_snake_case)]
 #[riscv_rt::entry]
 fn main() -> ! {
-    let config = if cfg!(feature = "beepy") {
-        mnemos_config::include_config!(mnemos_d1::PlatformConfig, "mq-pro-beepy")
-    } else {
-        mnemos_config::include_config!(mnemos_d1::PlatformConfig, "mq-pro")
-    }
-    .unwrap();
+    let config = mnemos_config::include_config!(mnemos_d1::PlatformConfig, "mq-pro").unwrap();
     mnemos_d1::kernel_entry(config);
 }
--- platforms/allwinner-d1/src/drivers/mod.rs
+++ platforms/allwinner-d1/src/drivers/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "i2c_puppet")]
+pub mod i2c_puppet;
+#[cfg(feature = "sharp-display")]
 pub mod sharp_display;
 pub mod spim;
 pub mod twi;
